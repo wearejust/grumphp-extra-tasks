0bbc000617022d60d648e83be1d464c54bdba76f
Cees van Egmond <cees.vanegmond@wearejust.com>
2016-09-04 22:53:28 +0200

    Feature: Be compatible with GrumPHP 0.9.2 - Remove methods which are not needed because of this new version and change of method visibility.

--- src/Task/PhpCsAutoFixer.php
+++ src/Task/PhpCsAutoFixer.php
@@ -2,8 +2,6 @@
 
 namespace Wearejust\GrumPHPExtra\Task;
 
-use GrumPHP\Collection\FilesCollection;
-use GrumPHP\Collection\ProcessArgumentsCollection;
 use GrumPHP\Runner\TaskResult;
 use GrumPHP\Task\Context\ContextInterface;
 use GrumPHP\Task\Context\RunContext;
@@ -49,65 +47,5 @@ class PhpCsAutoFixer extends PhpCsFixer
         }
 
         return $this->runOnChangedFiles($context, $arguments, $files);
-    }
-
-    /**
-     * @param ContextInterface           $context
-     * @param ProcessArgumentsCollection $arguments
-     *
-     * @return TaskResult
-     */
-    private function runOnAllFiles(ContextInterface $context, ProcessArgumentsCollection $arguments)
-    {
-        $process = $this->processBuilder->buildProcess($arguments);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            $messages = array($this->formatter->format($process));
-            $suggestions = array($this->formatter->formatSuggestion($process));
-            $errorMessage = $this->formatter->formatErrorMessage($messages, $suggestions);
-
-            return TaskResult::createFailed($this, $context, $errorMessage);
-        }
-
-        return TaskResult::createPassed($this, $context);
-    }
-
-    /**
-     * @param ContextInterface           $context
-     * @param ProcessArgumentsCollection $arguments
-     * @param FilesCollection            $files
-     *
-     * @return TaskResult
-     */
-    private function runOnChangedFiles(
-        ContextInterface $context,
-        ProcessArgumentsCollection $arguments,
-        FilesCollection $files
-    ) {
-        $hasErrors = false;
-        $messages = array();
-        $suggestions = array();
-
-        foreach ($files as $file) {
-            $fileArguments = new ProcessArgumentsCollection($arguments->getValues());
-            $fileArguments->add($file);
-            $process = $this->processBuilder->buildProcess($fileArguments);
-            $process->run();
-
-            if (!$process->isSuccessful()) {
-                $hasErrors = true;
-                $messages[] = $this->formatter->format($process);
-                $suggestions[] = $this->formatter->formatSuggestion($process);
-            }
-        }
-
-        if ($hasErrors) {
-            $errorMessage = $this->formatter->formatErrorMessage($messages, $suggestions);
-
-            return TaskResult::createFailed($this, $context, $errorMessage);
-        }
-
-        return TaskResult::createPassed($this, $context);
     }
 }
